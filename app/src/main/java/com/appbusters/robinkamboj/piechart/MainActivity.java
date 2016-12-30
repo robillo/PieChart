68d8d9654413e4d3658faf808705e0c64869113a
Robin Kamboj <dev.robinkamboj@gmail.com>
2016-12-30 08:24:34 +0530

    better looks

--- app/src/main/java/com/appbusters/robinkamboj/piechart/MainActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/piechart/MainActivity.java
@@ -39,19 +39,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         context= getApplicationContext();
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         pieChart= (PieChart) findViewById(R.id.pieChart);
 

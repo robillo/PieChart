da0be6c0cd0ded73da3c6b3ae31f89f4212e04e1
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-01-06 23:12:45 +0530

    twitches

--- app/src/main/java/com/appbusters/robinkamboj/piechart/MainActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/piechart/MainActivity.java
@@ -7,8 +7,6 @@ import android.graphics.Color;
 import android.graphics.EmbossMaskFilter;
 import android.graphics.PointF;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MotionEvent;
